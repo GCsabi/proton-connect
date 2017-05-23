389c5884b7f6a540c99076041ab68874a64280d7
albalitz <albalitz@fb3.uni-bremen.de>
2017-05-23 21:54:51 +0200

    Add `init`

--- proton-connect.py
+++ proton-connect.py
@@ -1,15 +1,58 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import stat
+import requests
+from getpass import getpass
+from zipfile import ZipFile
+
+import sh
 
 
 HOME_DIR = os.path.expanduser("~")
 CONFIG_DIR = os.path.join(HOME_DIR, ".proton-connect")
 
+config_url = "https://protonvpn.com/download/ProtonVPN_config.zip"
+vpn_config_dir = os.path.join(CONFIG_DIR, "ProtonVPN_config")
+user_config = os.path.join(CONFIG_DIR, "protonvpn.user")
 
 
 def init():
-    pass
+    os.makedirs(CONFIG_DIR, exist_ok=True)
+
+    print("Downloading ProtonVPN config files ... ", end = "")
+    r = requests.get(config_url, stream=True)
+    r.raise_for_status()
+    zipfile_path = os.path.join(CONFIG_DIR, "ProtonVPN_config.zip")
+    with open(zipfile_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=256):
+            f.write(chunk)
+    ZipFile(zipfile_path).extractall(CONFIG_DIR)
+    os.remove(zipfile_path)
+    print("done.")
+
+    print("Do you want to save your login data for the ProtonVPN? ", end = "")
+    want_to_save_login = input("[yes/no] ").lower()
+    if want_to_save_login != "yes":
+        return
+
+    login = input("ProtonVPN login: ")
+    password = getpass("ProtonVPN password (leave blank to not save): ")
+
+    if os.path.exists(user_config):
+        print(f"A user configuration already exists ({user_config}).")
+        overwrite = input("Overwrite? [yes/no] ").lower()
+        if overwrite != "yes":
+            return
+
+    with open(user_config, "w") as f:
+        f.write(f"{login}\n")
+        f.write(f"{password}\n")
+    sh.chmod("-R", 700, CONFIG_DIR)
+    sh.chmod(600, user_config)
+    print(f"Saved to {user_config}")
+
+    print("proton-connect is now ready to use.")
 
 
 def available(country=None):
